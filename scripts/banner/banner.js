b82c25c116e79e42a176f2e2ca5c3a3aed10e29f
Michael Cortez <139833375+MikesTheCodingAddict@users.noreply.github.com>
2026-08-19 00:36:47 +0000

    Began writing the tablet viewport styling code in CSS and introduced CSS Grid support for my website. These changes are half-baked in this push update only because resources were exhausted on the current Codespace used for the development of this project. A new Codespace will be created due to the strong necessity of needing to finish this project within a certain amount of time, ideally by the end of August 2026. All aforementioned updates here will be finished in the very next committed push.

--- scripts/banner/banner.js
+++ scripts/banner/banner.js
@@ -5,15 +5,15 @@ banner.innerHTML = "<b><p>This website is currently under construction. Some con
 
 // Styling the banner.
 banner.style.display = 'block'; // Determines the visibility of the banner.
-banner.style.top = '0';
-banner.style.left = '0';
+banner.style.top = '0px';
+banner.style.left = '0px';
+banner.style.right = '0px';
 banner.style.width = '100%';
 banner.style.backgroundColor = 'yellow';
 banner.style.color = 'black';
 banner.style.textAlign = 'center';
 banner.style.padding = '10px 10px 10px 10px';
 banner.style.justifySelf = 'center';
-banner.style.transform = 'translateY(-4%)';
 
-// Prepending the banner to the webpage.
+// Prepending the banner to the top of the webpage's body.
 document.body.prepend(banner);
--- scripts/phone/phoneText.js
+++ scripts/phone/phoneText.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
+	const countCode = "+1";
 	const area = "774";
 	const middle = "205";
 	const line = "1787";
-	const full = area + middle + line;
+	const full = countCode + area + middle + line;
 	const link = document.getElementById("phoneTextLink");
 
 	link.href = `sms:${full}`;
